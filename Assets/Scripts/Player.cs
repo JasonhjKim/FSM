0ce1770a68bde1633f660ce3e0a24fa13837f259
Hyun Jun Kim <jason@Hyuns-MacBook-Pro.local>
2017-08-05 12:50:47 -0700

    First commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	private FSM fsm;
+	// Use this for initialization
+	void Start () {
+		initalizeAllState ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		fsm.CurrentState.UpdateState ();
+	}
+
+	private void initalizeAllState() {
+		fsm = new FSM ();
+		WanderingState ws = new WanderingState (1);
+		fsm.AddState (ws);
+		//and so on
+	}
+}
